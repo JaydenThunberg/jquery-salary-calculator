d8d040fe8df99cd3ca5da47d6398ea0f67c27176
Jayden Thunberg <jlthunberg@gmail.com>
2020-04-11 12:10:47 -0500

    addEmployee started/ btn working

--- scripts/client.js
+++ scripts/client.js
@@ -1,7 +1,28 @@
 console.log('js');
 
 $(document).ready(readyCalculator)
+let employees=[];
     
 function readyCalculator () {
     console.log('jQ')
+    $('#submitEmpBtn').on('click', addEmployee)
 };
+
+//submit button append employee to dom
+function addEmployee(){
+    //target input elements by ID
+    //get value of text input
+    let empFirst = $( '#firstNameInput' ).val();
+    let empLast = $( '#lastNameInput' ).val();
+    let empId = $( '#idInput' ).val();
+    let empTitle = $( '#titleInput' ).val()
+    let empSalary = $( '#salaryInput' ).val()
+    console.log(empFirst, empLast, empId, empTitle, empSalary);
+    
+    //empty input values after pushing into addEmployee
+    $( '#firstNameInput' ).val( '' );
+    $( '#lastNameInput' ).val( '' );
+    $( '#titleInput' ).val( '' );
+    $( '#salaryInput' ).val( '' );
+    $( '#modelInput' ).val( '' );
+  }//end addEmployee
